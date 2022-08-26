8e9da2b54c05021ad8c32f386fb3e95633a3d4e4
jeehee1 <jeehee1w@gmail.com>
2022-08-26 19:21:26 +0900

    connect mongo databse and make a group schema

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const engine = require('ejs-mate');
 const path = require('path')
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/studyingGroup');
+    console.log('CONNECTED ON MONGO DATABASE!')
+}
+
+const groupSchema = new mongoose.Schema({
+    title: String,
+    subject: String,
+    location: String,
+    meeting_times: Number,
+    online: {
+        type: String,
+        enum: ['y', 'n']
+    },
+    description: String
+})
+
+const Group = mongoose.model('Group', groupSchema);
 
 const app = express();
 
@@ -10,7 +32,9 @@ app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('show');
+    res.render('index');
 })
 
-app.listen(3000);
+app.listen(3000, (req, res) => {
+    console.log('CONNECTED ON PORT 3000');
+});
